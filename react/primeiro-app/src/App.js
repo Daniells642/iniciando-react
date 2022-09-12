5947ff54c7d39248deca33b0fc6a2687eb0aa387
unknown <daniells642@hotmail.com>
2022-09-12 13:14:09 -0300

    Componentes
    criando os primeiros componentes do app.

--- react/primeiro-app/src/App.js
+++ react/primeiro-app/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from './componentes/Header/';
 import Footer from './componentes/Footer/';
+import HomePage from './componentes/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Footer/>
+      <HomePage/>
     </div>
   );
 }

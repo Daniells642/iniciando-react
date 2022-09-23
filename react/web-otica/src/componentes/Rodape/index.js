f82f6a979ed38e80b0a4e452230b26a58218cc1a
unknown <daniells642@hotmail.com>
2022-09-23 11:11:04 -0300

    Finalizando os estilos da página

--- react/web-otica/src/componentes/Rodape/index.js
+++ react/web-otica/src/componentes/Rodape/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './estilos.css';
 
 export default function Rodape() {
   return (
--- react/web-otica/src/componentes/SecaoContato/index.js
+++ react/web-otica/src/componentes/SecaoContato/index.js
@@ -1,30 +1,45 @@
 import React from 'react';
-/*import './estilo.css';*/
+import './estilos.css';
 
 export default function SecaoContato() {
   return (
-    <section>
-       <div id="contato">
+    <section id="contato">
+       <div className='limitar-secao secao-contato'>
+
             <h2>Fale conosco</h2>
-            <p>Não perca tempo, venha conhecer uma de nossas lojas ou entre em contato através de nossas redes sociais ou da central de atendimento.
-            </p>
-            <div>
+            <p>Não perca tempo, venha conhecer uma de nossas lojas ou entre em contato através de nossas redes sociais ou da central de atendimento.</p>
+
+            <div className='container-contato'>
+              <div className="contato-conteudo">
+                <h4>Contato</h4>
                 <div>
-                    <h3>Contato</h3>
-                    <img src='../assets/local.png' alt=""/><p>Nova Iguaçu, RJ</p>
-                    <img src='../assets/telefone.png' alt=""/><p>(21) 9999-9999</p>
-                    <img src='../assets/email.png' alt=""/><p>contato@oticavida.com</p>
-                    
-                   
+                    <img src="assets/local.png" title="Ícone local" alt="Pino de localização"/>
+                    <span>Nova Iguaçu, RJ</span>
                 </div>
-                    
                 <div>
-                    <h3>Nossas Redes sociais</h3>
-                    <img src='../assets/fb.png' alt=""/><p>/OticaVida</p>
-                    <img src='../assets/ig.png' alt=""/><p>@oticavidarj</p>
-                    <img src='../assets/tt.png' alt=""/><p>@oticavidarj</p>
-                    
-                </div> 
+                    <img src="assets/telefone.png" title="Ícone telefone" alt="Ícone de um telefone"/>
+                    <span>(21) 9999-9999</span>
+                </div>
+                <div>
+                    <img src="assets/email.png" title="Ícone email" alt="Ícone de uma carta"/>
+                    <span>contato@oticavida.com</span>
+                </div>
+            </div>
+            <div className="contato-conteudo">
+                <h4>Nossas Redes Sociais</h4>
+                <div>
+                    <img src="assets/fb.png" title="Ícone facebook" alt="logo do facebook"/>
+                    <span>/OticaVida</span>
+                </div>
+                <div>
+                    <img src="assets/ig.png" title="Ícone instagram" alt="logo do instagram"/>
+                    <span>@oticavidarj</span>
+                </div>
+                <div>
+                    <img src="assets/tt.png" title="Ícone twitter" alt="logo do twitter"/>
+                    <span>@oticavidarj</span>
+                </div>
+            </div>
 
             </div>
         </div>
